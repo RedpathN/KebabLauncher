928df9a8d3feee2d7b00b488c67dd85cd9da4094
Nick <redpathn@gmail.com>
2020-06-30 14:44:10 +1200

    Initial Commit

---
+++ Assets/Scripts/ConveyorController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public List<GameObject> conveyorPositions;
+    public bool isLooping;
+
+    private LineRenderer lr;
+
+    private void Awake() => lr = GameObject.Find("ConveyorLine").GetComponent<LineRenderer>();
+
+    private void Start()
+    {
+        lr.positionCount = isLooping ? conveyorPositions.Count + 1 : conveyorPositions.Count;
+        for(int i = 0; i < conveyorPositions.Count; i++)
+        {
+            
+            lr.SetPosition(i, conveyorPositions[ i % lr.positionCount].transform.position);
+        }
+        
+    }
+}
---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+
+    public float speed = 5;
+
+    private bool onConveyor = false;
+    private int checkpoint = 0;
+    private float progress;
+
+    private ConveyorController cc;
+
+    private Vector3 posA;
+    private Vector3 posB;
+    private float pointDist;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GameObject.FindObjectOfType<ConveyorController>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Conveyor")
+        {
+            getPoints();
+            onConveyor = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Conveyor")
+        {
+            onConveyor = false;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (onConveyor)
+        {
+            progress += (1/pointDist) * (speed * Time.fixedDeltaTime);
+            gameObject.transform.position = Vector3.Lerp(posA, posB, progress );
+            if(progress >= 1)
+            {
+                checkpoint++;
+                if(cc.isLooping || checkpoint < cc.conveyorPositions.Count - 1)
+                {
+                    getPoints();
+                }
+                
+                    
+            }
+        }
+
+    }
+
+    void getPoints() 
+    {
+        progress = 0;
+        pointDist = 0;
+        posA = cc.conveyorPositions [checkpoint % cc.conveyorPositions.Count].transform.position;
+        posB = cc.conveyorPositions [(checkpoint + 1) % cc.conveyorPositions.Count].transform.position;
+        pointDist = Vector3.Distance(posA, posB);
+    }
+}
